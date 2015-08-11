3f0aa14c42a3cb0f4e22a64b44a3fc0377b20bcb
Daniel Suchkov <suc-daniil@yandex.ru>
2015-08-12 03:10:49 +0800

    first implementation of reports generator

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <exception>
+#include <cstring>
+#include <memory>
+
+
+#include <unordered_map>
+
+/*!
+ * \brief The ValStorage class is example of ValSource
+ */
+class ValStorage {
+public:
+    ValStorage() {
+        for (char i = 'a'; i <= 'z'; ++i) {
+            vals[std::string{i}] = std::string{i - 'a' + '0'};
+        }
+    }
+
+    /// Have to return string representation of value. If no such value – throw std::out_of_range.
+    std::string get_val(const std::string &name) const {
+        return vals.at(name);
+    }
+
+private:
+    std::unordered_map<std::string, std::string> vals;
+};
+
+template< class ValSource>
+class Generator {
+public:
+    Generator(
+            const std::string &template_path,
+            const ValSource &src,
+            const char *open_delim,
+            const char *close_delim
+    )
+        : templ_file(template_path)
+        , src(src)
+        , open_delim(open_delim)
+        , close_delim(close_delim) {
+        if (!templ_file) {
+            throw std::ios_base::failure("cannot open file [" + template_path + "]");
+        }
+    }
+
+    void generate_file(const std::string &output_path) {
+        std::ofstream of(output_path);
+        if (!of) {
+            throw std::ios_base::failure("cannot create file [" + output_path + "]");
+        }
+        std::string line_buf; // to avoid reallocations
+        std::string name_buf; // same purpose
+
+        std::getline(templ_file, line_buf);
+        while (templ_file.good()) {
+            size_t s_pos = 0;
+            while ((s_pos = line_buf.find(open_delim, s_pos)) != std::string::npos) {
+                auto e_pos = line_buf.find(close_delim, s_pos);
+                if (e_pos == std::string::npos) {
+                    break;
+                }
+
+                name_buf.assign(line_buf.begin() + s_pos + strlen(open_delim), line_buf.begin() + e_pos);
+                auto pattern_length = name_buf.size() + strlen(open_delim) + strlen(close_delim);
+                try {
+                    std::string val = src.get_val(name_buf);
+                    line_buf.replace(s_pos, pattern_length, val);
+                    s_pos += val.length();
+                }
+                catch (const std::out_of_range &) {
+                    std::cout << "no such value: " << name_buf << std::endl;
+                    s_pos += strlen(open_delim);
+                }
+            }
+            of.write(line_buf.c_str(), line_buf.size());
+            of.put('\n');
+            std::getline(templ_file, line_buf);
+        }
+        templ_file.seekg(0, templ_file.beg);
+    }
+
+private:
+    std::ifstream templ_file;
+    const ValSource &src;
+    const char *open_delim;
+    const char *close_delim;
+};
+
+/*!
+ * To deduce type of ValSource automatically
+ */
+template< class ValSource >
+std::unique_ptr<Generator<ValSource>> make_generator(
+        const std::string &template_path,
+        const ValSource &src,
+        const char *open_delim = "<<<",
+        const char *close_delim = ">>>") {
+    return std::make_unique<Generator<ValSource>>(template_path, src, open_delim, close_delim);
+}
+
+int main() {
+    try {
+        ValStorage stg;
+        auto gnr = make_generator(std::string("/home/fallen/ttemplate"), stg);
+        gnr->generate_file("/home/fallen/treport");
+    }
+    catch (const std::exception &err) {
+        std::cout << err.what() << std::endl;
+    }
+}
---
+++ repgen.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-08-12T00:02:51
+#
+#-------------------------------------------------
+
+QT       -= gui
+
+TARGET = repgen
+CONFIG   += console \
+            c++11
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+unix {
+    INCLUDEPATH += /home/fallen/data/programing/c++/lib/boost
+    LIBPATH = /home/fallen/data/programing/c++/lib/boost/stage/lib/
+    LIBS += $${LIBPATH}libboost_filesystem.a \
+        $${LIBPATH}libboost_system.a
+}
