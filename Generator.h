929c4a7b79697cd0d49447578376672563204754
Daniel Suchkov <suc-daniil@yandex.ru>
2015-08-31 22:05:06 +0600

    implement getting value from fields of records

--- Generator.h
+++ Generator.h
@@ -5,16 +5,27 @@
 #include <exception>
 #include <cstring>
 #include <memory>
+#include <unordered_map>
+#include <functional>
+#include <tuple>
 
-template< class ValSource>
+class WrongFormat : public std::exception {
+public:
+    WrongFormat(const std::string &msg)
+        : msg("Wrong format" + std::string(msg.empty() ? "" : ": ") + msg) {}
+
+private:
+    std::string msg;
+};
+
+template<class ValSource>
 class Generator {
 public:
     Generator(
             const std::string &template_path,
             const ValSource &src,
             const char *open_delim,
-            const char *close_delim
-    )
+            const char *close_delim)
         : templ_file(template_path)
         , src(src)
         , open_delim(open_delim)
@@ -22,6 +33,13 @@ public:
         if (!templ_file) {
             throw std::ios_base::failure("cannot open file [" + template_path + "]");
         }
+
+        access_field = {
+            { "Name", &ValSource::get_name },
+            { "Value", &ValSource::get_value },
+            { "Description", &ValSource::get_description },
+            { "Unit", &ValSource::get_unit }
+        };
     }
 
     void generate_file(const std::string &output_path) {
@@ -41,15 +59,20 @@ public:
                     break;
                 }
 
+                /// \todo Rewrite it using std::string::substr
                 name_buf.assign(line_buf.begin() + s_pos + strlen(open_delim), line_buf.begin() + e_pos); // get pattern name
                 auto pattern_length = name_buf.size() + strlen(open_delim) + strlen(close_delim);
                 try { // replace pattern with value
-                    std::string val = src.get_val(name_buf);
+                    auto val = get_field_value(name_buf);
                     line_buf.replace(s_pos, pattern_length, val);
                     s_pos += val.length();
                 }
-                catch (const std::out_of_range &) {
-                    std::cout << "no such value: " << name_buf << std::endl;
+                catch (const std::out_of_range &err) {
+                    std::cout << err.what() << "; cannot get value: " << name_buf << std::endl;
+                    s_pos += strlen(open_delim);
+                }
+                catch (const WrongFormat &err) {
+                    std::cout << err.what() << " [" << name_buf << "]" << std::endl;
                     s_pos += strlen(open_delim);
                 }
             }
@@ -65,6 +88,31 @@ private:
     const ValSource &src;
     const char *open_delim;
     const char *close_delim;
+    std::unordered_map<
+        std::string, std::function<std::string (const ValSource *, const std::string &)>
+    > access_field;
+
+private:
+    /// \param record_field must be like this: FieldName(RecordName)
+    std::string get_field_value(const std::string &placeholder) const {
+        std::string record, field;
+        std::tie(record, field) = parse_placeholder(placeholder);
+        auto it = access_field.find(field);
+        if (it == access_field.end()) {
+            throw std::out_of_range("no such field: " + field);
+        }
+        std::cout << record << ": " << field << std::endl;
+        return it->second(&src, record);
+    }
+
+    static auto parse_placeholder(const std::string &placeholder)
+        -> std::tuple<std::string, std::string> {
+        auto rs = placeholder.find('(');
+        if (rs == std::string::npos) { throw WrongFormat("cannot find opening '('"); }
+        auto re = placeholder.find(')', rs);
+        if (re == std::string::npos) { throw WrongFormat("cannot find closing ')'"); }
+        return std::make_tuple(placeholder.substr(rs + 1, re - rs - 1), placeholder.substr(0, rs));
+    }
 };
 
 /*!
--- main.cpp
+++ main.cpp
@@ -3,24 +3,57 @@
 
 #include "Generator.h"
 
+struct Record {
+    std::string name;
+    int value;
+    std::string description;
+    unsigned unit;
+};
+
 /*!
  * \brief The ValStorage class is example of ValSource
  */
 class ValStorage {
 public:
     ValStorage() {
-        for (char i = 'a'; i <= 'z'; ++i) {
-            vals[std::string{i}] = std::string{i - 'a' + '0'};
-        }
+        vals = {
+            { "The sandwich-maker", Record { "Arthur Dent", 29, "Human, Male", 42 } },
+            { "Ix", Record { "Ford Prefect", 36, "Betelgeusian, Male", 13 } },
+            { "Trillian Astra", Record { "Tricia Marie McMillan", 32, "Human, Female", 57 } },
+            { "Slartibartfast", Record { "Slartibartfast", 52, "Magrathean, Male", 61 } },
+            { "Marvin", Record { "the Paranoid Android", 60, "GPP prototype android, Male design", 18 } },
+            { "Zaphod Beeblebrox", Record { "President Zaphod Beeblebrox", 31, "Betelgeusian, Male", 27 } }
+        };
     }
 
     /// Have to return string representation of value. If no such value – throw std::out_of_range.
-    std::string get_val(const std::string &name) const {
-        return vals.at(name);
+//    std::string get_val(const std::string &name) const {
+//        return vals.at(name);
+//    }
+
+    std::string get_name(const std::string &name) const {
+        return get_record(name).name;
+    }
+
+    std::string get_value(const std::string &name) const {
+        return std::to_string(get_record(name).value);
+    }
+
+    std::string get_description(const std::string &name) const {
+        return get_record(name).description;
+    }
+
+    std::string get_unit(const std::string &name) const {
+        return std::to_string(get_record(name).unit);
     }
 
 private:
-    std::unordered_map<std::string, std::string> vals;
+    std::unordered_map<std::string, Record> vals;
+
+private:
+    const Record &get_record(const std::string &name) const {
+        return vals.at(name);
+    }
 };
 
 int main() {
